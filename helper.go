947894d2ff5363304cbb6aa0fb036d9688de360d
GalvinGao <me@galvingao.com>
2021-12-04 13:27:46 +0800

    feat: add StartTracerFromCtx to helper functions

--- helper.go
+++ helper.go
@@ -12,6 +12,10 @@ func FromCtx(ctx *fiber.Ctx) context.Context {
 	return otelCtx
 }
 
+func StartTracerFromCtx(ctx *fiber.Ctx, spanName string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
+	return Tracer.Start(FromCtx(ctx), spanName, opts...)
+}
+
 func SpanFromCtx(ctx *fiber.Ctx) trace.Span {
 	return trace.SpanFromContext(FromCtx(ctx))
 }
